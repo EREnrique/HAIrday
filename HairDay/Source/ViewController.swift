f1fa181baad7e91400ced024e0c7b542933fd609
Enrico Boller <email.prof.er@gmail.com>
2018-05-14 18:18:40 +0800

    Committed Project Initial Resources for Hairday

---
+++ HairDay/Source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HairDay
+//
+//  Created by Enrico Boller on 10/05/2018.
+//  Copyright © 2018 Enrico Boller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
